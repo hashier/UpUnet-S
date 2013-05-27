52b1a11354e5de4478e8f262fa4ae36938fddabb
Christopher Loessl <cloessl@x-berg.de>
2013-05-27 22:24:26 +0200

    First idea how it should look like
    no functionality

--- UpUnet-S/MainViewController.m
+++ UpUnet-S/MainViewController.m
@@ -18,12 +18,25 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"viewDidLoad");
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    NSLog(@"viewDidAppear");
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)disconnectButton:(UIButton *)sender {
+}
+
+- (IBAction)connectButton:(UIButton *)sender {
 }
 
 @end
